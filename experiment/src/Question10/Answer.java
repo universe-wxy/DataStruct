a6713534a1d6a1e0a56c28fd1fbe07a40a710e4a
王熙宇 <2537239220@qq.com>
2022-12-14 21:37:29 +0800

    更正Question10

--- experiment/src/Question10/Answer.java
+++ experiment/src/Question10/Answer.java
@@ -1,18 +1,22 @@
 package Question10;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Answer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        InputStream stream=new FileInputStream("C:\\Users\\25372\\Desktop\\workspace\\数据结构\\experiment\\src\\Question10\\in.txt");
+        Scanner reader=new Scanner(stream);
         Hash list = new Hash(100);
-        Scanner reader = new Scanner(System.in);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 5; i++) {
             int row, col;
             float val;
             row = reader.nextInt();
             col = reader.nextInt();
             val = reader.nextFloat();
             list.add(row, col, val);
+            System.out.print(list.index(val)+" ");
         }
+
     }
 }
--- experiment/src/Question10/Hash.java
+++ experiment/src/Question10/Hash.java
@@ -1,17 +1,29 @@
 package Question10;
 
+import java.util.ArrayList;
+
 public class Hash {
     int m;
     DataType[] H=new DataType[130];
+    ArrayList<Integer> list;
     Hash(int m){
         this.m=m;
         for(int i=0;i<130;i++)
             H[i]=null;
+        list=new ArrayList<>();
     }
     void add(int row,int col,float val){
         int pos=(13*row+17*col)%m;
         while(H[pos]!=null)
             pos=(pos+1)%m;
         H[pos]=new DataType(row,col,val);
+        list.add(pos);
+    }
+    int index(float val){
+        for (Integer integer : list) {
+            if (H[integer].val == val)
+                return integer;
+        }
+        return -1;
     }
 }
